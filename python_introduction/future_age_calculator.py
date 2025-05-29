3ad564557a2a926bb02d2d415b4d60e0bc6ba48f
DennisKavuwa <bkavuwa@gmail.com>
2025-05-29 17:38:52 +0300

    Create a future age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+#Creating a future age calculator
+current_age = int(input("How old are you? "))
+
+#calculations of future age
+future_age = current_age + 27
+
+#output
+print(f"in 2050 you will be {future_age} years old")
