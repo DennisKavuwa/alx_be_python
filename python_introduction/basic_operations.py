09f72aec08a9b6b18f3a63bd4ea99cb1dbfcef18
DennisKavuwa <bkavuwa@gmail.com>
2025-05-29 17:10:55 +0300

    Add basic_operation.py script for ALX

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#basic_operations.py
+number1 = 10
+number2 = 5
+
+#calculations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#output
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"SUbtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
